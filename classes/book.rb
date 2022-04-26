c3af3946ed6ece2807d32a7fde5aa306169483f8
Gegardus <stgegard@gmail.com>
2022-04-26 16:12:48 +0400

    Add Book and Label classes

---
+++ classes/book.rb
@@ -0,0 +1,17 @@
+require_relative './item'
+
+class Book < Item
+  attr_accessor :publisher, :cover_state
+
+  def initialize(name, publish_date, publisher, cover_state, archived = false, id = Random.rand(1..10_000))
+    super(name, publish_date, archived, id)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  private
+
+  def can_be_archived?
+    super || @cover_state == 'bad'
+  end
+end
