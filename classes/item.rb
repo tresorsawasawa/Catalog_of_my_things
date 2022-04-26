df413514708795e7af3dd5fbac1430f929ea1c39
ahmed tawfik ali <a.alrakhawy@gmail.com>
2022-04-26 05:02:48 +0200

    Created item.rb

---
+++ classes/item.rb
@@ -0,0 +1,24 @@
+require 'date'
+
+class Item
+  attr_accessor :publish_date, :genre, :author, :source, :label
+  attr_reader :id, :archived
+
+  def initialize(publish_date, archived)
+    @id = Random.rand(1..1000)
+    @publish_date = publish_date
+    @archived = archived
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+
+  private
+
+  def can_be_archived?
+    now = Date.today.year
+    published = publish_date.year
+    (now - published) >= 10
+  end
+end
