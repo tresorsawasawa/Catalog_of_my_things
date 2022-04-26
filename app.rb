8557f8501aec099d1994f6d4392c5ea0b19a86e1
Abdulfatai Badara <fobadara@gmail.com>
2022-04-26 11:03:06 +0100

    Impement code for app entry

---
+++ app.rb
@@ -0,0 +1,47 @@
+class App
+  attr_accessor :books, :games, :authors, :music_albums
+
+  def list_all_books
+    puts 'list all books'
+  end
+
+  def add_a_book
+    puts 'add a book'
+  end
+
+  def list_all_labels
+    puts 'list all labels'
+  end
+
+  def save_book
+    puts 'save_book'
+  end
+
+  def load_book
+    puts 'Load book'
+  end
+
+  def list_music_albums
+    puts 'list music albums'
+  end
+
+  def list_genres
+    puts 'list genres'
+  end
+
+  def add_music_album
+    puts 'add music album'
+  end
+
+  def list_games
+    puts 'list games'
+  end
+
+  def create_game
+    puts 'create game'
+  end
+
+  def list_authors
+    puts 'list authors'
+  end
+end
