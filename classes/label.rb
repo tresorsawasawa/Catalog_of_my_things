f369ee361c86f51ba215410c3eb636e11c820a0f
Gegardus <stgegard@gmail.com>
2022-04-26 16:20:51 +0400

    Fix linter errors

--- classes/label.rb
+++ classes/label.rb
@@ -1,14 +1,14 @@
 class Label
   attr_accessor :title, :color
   attr_reader :id, :items
-  
+
   def initialize(title, color, id = Random.rand(1..100_000))
     @id = id
     @title = title
     @color = color
     @items = []
   end
-  
+
   def add_item(item)
     @items << item
     item.label = self
