50851dd01097669935109815e052ee1ecc656e5f
Gegardus <stgegard@gmail.com>
2022-04-27 23:12:37 +0400

    Add options for book and label

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
-require_relative './console/display_music_albums'
+# require_relative './console/display_music_albums'
+require_relative './classes/book'
+require_relative './classes/label'
 
 class App
   attr_accessor :books, :games, :authors, :music_albums
@@ -14,19 +16,38 @@ class App
   end
 
   def list_all_books
-    puts 'list all books'
+    @books.each do |book|
+      puts "Title: #{book.title}, Publisher: #{book.publisher},
+      Cover state: #{book.cover_state}, Date of Publish: #{book.publish_date}"
+    end
   end
 
   def add_a_book
-    puts 'add a book'
+    puts 'Please write in title: '
+    title = gets.chomp
+
+    puts 'Please write in publisher: '
+    publisher = gets.chomp
+
+    puts 'Please write in cover state ["good"/"bad"]'
+    cover_state = gets.chomp
+
+    puts 'Please write in date of publish [Enter date in format (yyyy-mm-dd)]'
+    publish_date = gets.chomp
+
+    @books.push(Book.new(title, publisher, cover_state, publish_date))
+    puts 'Book is created'
+  end
+
+  def list_all_labels
+    puts 'There are no labels available' if @labels.empty?
+    @labels.each do |label|
+      puts "Title: #{label.title}, Color: #{label.color}"
+    end
   end
 
   def add_a_movie
     puts 'add a book'
-  end
-
-  def list_all_labels
-    puts 'list all labels'
   end
 
   def save_book
