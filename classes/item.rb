13e5f8d5aba732a2673340461e90beaab1606f3d
Gegardus <stgegard@gmail.com>
2022-04-27 13:04:44 +0400

    Add setters to item

--- classes/item.rb
+++ classes/item.rb
@@ -10,6 +10,26 @@ class Item
     @archived = archived
   end
 
+  def genre=(genre)
+    @genre = genre
+    @genre.items.push(self) unless @genre.items.include?(self)
+  end
+
+  def author=(author)
+    @author = author
+    @author.items.push(self) unless @author.items.include?(self)
+  end
+
+  def label=(label)
+    @label = label
+    @label.items.push(self) unless @label.items.include?(self)
+  end
+
+  def source=(source)
+    @source = source
+    @source.items.push(self) unless @source.items.include?(self)
+  end
+
   def move_to_archive
     @archived = true if can_be_archived?
   end
