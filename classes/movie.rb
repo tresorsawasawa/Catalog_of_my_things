322eac6469fa7e72b0946bfa4598d0902b7f5adf
ahmed tawfik ali <a.alrakhawy@gmail.com>
2022-04-29 01:42:05 +0200

    Fix Rspec error

--- classes/movie.rb
+++ classes/movie.rb
@@ -4,7 +4,7 @@ require_relative './item'
 class Movie < Item
   attr_accessor :silet
 
-  def initialize(silet, publish_date)
+  def initialize(silet, publish_date, archived)
     super(publish_date, archived)
     @silet = silet
   end
